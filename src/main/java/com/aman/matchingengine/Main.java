c7d38cd5ac741468f209b81296822e96602ff195
Aman-Tripathi-Code <tripathiaman555@gmail.com>
2026-05-07 05:18:36 +0000

    Implemented in-memory OrderBook

--- src/main/java/com/aman/matchingengine/Main.java
+++ src/main/java/com/aman/matchingengine/Main.java
@@ -1,36 +1,60 @@
 package com.aman.matchingengine;
 
 
+import com.aman.matchingengine.engine.OrderBook;
 import com.aman.matchingengine.model.Order;
 import com.aman.matchingengine.model.OrderSide;
-import com.aman.matchingengine.model.Trade;
 
 import java.time.Instant;
 
 public class Main {
     public static void main(String[] args) {
-        Order order = new Order(
-                "ORD-1",
+        OrderBook orderBook = new OrderBook("AAPL");
+
+        orderBook.addOrder(new Order(
+            "B-1",
+            "AAPL",
+            OrderSide.BUY,
+            18550,
+            100,
+            1,
+            Instant.now()
+        ));
+
+        orderBook.addOrder(new Order(
+                "B-2",
                 "AAPL",
                 OrderSide.BUY,
-                18550,
-                100,
-                1,
+                18600,
+                50,
+                2,
                 Instant.now()
-        );
+        ));
 
-        Trade trade = new Trade(
-                "TRD-1",
+        orderBook.addOrder(new Order(
+                "S-1",
                 "AAPL",
-                "ORD-1",
-                "ORD-2",
-                18550,
-                40,
+                OrderSide.SELL,
+                18650,
+                70,
+                3,
                 Instant.now()
-        );
-        System.out.println("Day 1 setup successful");
-        System.out.println(order);
-        System.out.println(trade);
+        ));
+
+        orderBook.addOrder(new Order(
+                "S-2",
+                "AAPL",
+                OrderSide.SELL,
+                18620,
+                30,
+                4,
+                Instant.now()
+        ));
+
+        System.out.println("Best Bid: " + orderBook.peekBestBid());
+        System.out.println("Best Ask: " + orderBook.peekBestAsk());
+        System.out.println();
+        System.out.println(orderBook.printBook());
 
     }
 }
